7b640420c777cacefe4c035eeaccde7080f6cbc1
buistvo <plus100500@gmail.com>
2024-02-24 15:38:42 +0300

    add genre select

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import './App.css';
 import { Counter } from './components/Counter/Counter';
+import { GenreSelect } from './components/GenreSelect/GenreSelect';
 import { Search } from './components/Search/Search';
+
+const GENRE_LIST = ['ALL', 'DOCUMENTARY', 'COMEDY', 'HORROR', 'CRIME'];
 
 function App() {
   return (
     <>
       <Counter initialCount={1}></Counter>
       <Search onSearch={(query) => console.log(query)} />
+      <GenreSelect
+        genreList={GENRE_LIST}
+        onSelect={(genre) => console.log(genre)}
+      />
     </>
   );
 }
