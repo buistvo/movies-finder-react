3f109c852b7702cb2ffdcb5bc76632ef97b5cf0c
buistvo <plus100500@gmail.com>
2024-03-06 00:32:16 +0300

    context menu

--- src/components/MovieTile/MovieTile.tsx
+++ src/components/MovieTile/MovieTile.tsx
@@ -12,6 +12,7 @@ const Container = styled.div`
   display: block;
   width: fit-content;
   height: fit-content;
+  position: relative;
   color: ${Colors.SecondaryText};
 `;
 
@@ -37,11 +38,53 @@ const GenreList = styled.div`
   font-size: 0.9rem;
   font-weight: 500;
 `;
+
+const ContextMenu = styled.div`
+  position: absolute;
+  right: 0;
+`;
+
+const ContextMenuButton = styled.button`
+  border-radius: 40px;
+  width: 40px;
+  height: 40px;
+  margin: 0.5rem;
+  background-color: ${Colors.Workspace};
+`;
+
+const Ellipsis = styled.span`
+  position: absolute;
+  top: 50%;
+  right: 18px;
+  transform: translateY(-50%);
+  color: white;
+  writing-mode: vertical-lr;
+  font-size: 2em;
+  width: 20px;
+`;
+
+const StyledEllipsis = () => <Ellipsis>&#x2026;</Ellipsis>;
+
 export function MovieTile({ movie, onClick }: MovieTileProps) {
   const [movieState, setMovieState] = useState(movie || {});
+  const [isContextMenuOpenState, setIsContextMenuOpenState] = useState(false);
+
+  function onContextMenuClick() {
+    setIsContextMenuOpenState(true);
+  }
 
   return (
     <Container>
+      <ContextMenu>
+        {isContextMenuOpenState ? (
+          <div>context menu</div>
+        ) : (
+          <ContextMenuButton onClick={onContextMenuClick}>
+            <StyledEllipsis></StyledEllipsis>
+          </ContextMenuButton>
+        )}
+      </ContextMenu>
+
       <img src={movieState.imageUrl}></img>
       <MovieInfo>
         <Title> {movieState.name} </Title>
