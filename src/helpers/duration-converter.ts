8e0c9bdd0f615d8c7277a16b4ab3f9198fedf216
buistvo <plus100500@gmail.com>
2024-04-04 00:57:06 +0300

    fixed duration helper

--- src/helpers/duration-converter.ts
+++ src/helpers/duration-converter.ts
@@ -1,6 +1,5 @@
 export function convertDuration(duration: number) {
   const hrs = duration / 60;
-
   const mins = duration % 60;
-  return `${hrs.toFixed(0)}h ${mins}min`;
+  return `${Math.floor(hrs)}h ${mins}min`;
 }
